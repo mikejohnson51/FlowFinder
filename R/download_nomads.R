f8c293c05ed07d51338f489322c58754529d41be
mikejohnson51 <mike.johnson@geog.ucsb.edu>
2018-06-06 00:22:29 -0700

    fix download and data update

--- R/download_nomads.R
+++ R/download_nomads.R
@@ -1,11 +1,11 @@
 download_nomads = function(dir = './flowline-app/data/current_nc', fileList = NULL){
 
 for (i in seq_along(fileList[[2]])) {
-  if (!file.exists(paste0(dir, "/", basename(fileList[[2]])[i]))) {
+  if (!file.exists(paste0(dir, "/", basename(fileList[[2]][i])))) {
     download.file(
       url = fileList[[2]][i],
       mode = "wb",
-      destfile = paste0(dir, "/", paste0(fileList[[1]], basename(fileList[[2]])[i]))
+      destfile = paste0(dir, "/", paste0(fileList[[1]], basename(fileList[[2]][i])))
     )
   }
 }
