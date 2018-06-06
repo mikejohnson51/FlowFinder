a189480eedb5ca31ef2f7be374916d45c8771189
mikejohnson51 <mike.johnson@geog.ucsb.edu>
2018-06-06 11:52:28 -0700

    update download.Rmd

--- R/download_nomads_rda.R
+++ R/download_nomads_rda.R
@@ -1,16 +1,14 @@
-download_nomads_rda = function(dir = './flowline-app/data/current_nc', fileList = NULL){
+download_nomads_rda = function(fileList = NULL){
   
   tmp = tempdir()
     
     for (i in seq_along(fileList[[3]])) {
-      if (!file.exists(paste0(dir, "/", basename(fileList[[3]][i])))) {
-        download.file(
+          download.file(
           url = fileList[[3]][i],
           mode = "wb",
           destfile = paste0(tmp, "/", paste0(fileList[[1]], basename(fileList[[3]][i])))
         )
       }
-    }
     
     all.files =  list.files(tmp, pattern = ".nc$", full.names = T)
     
