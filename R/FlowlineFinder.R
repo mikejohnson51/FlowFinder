f3a89019e0b1ee105711fa6de6a81ba9276d0c84
mikejohnson51 <mike.johnson@geog.ucsb.edu>
2018-07-02 17:17:35 -0600

    add global var fix

--- R/FlowlineFinder.R
+++ R/FlowlineFinder.R
@@ -24,3 +24,17 @@
 
 
 NULL
+
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if(getRversion() >= "2.15.1")  { utils::globalVariables(c("LAT", "LON",
+                                                          "State", "Longitude", "Latitude",
+                                                          "nid_cleaned",
+                                                          "usgsStations",
+                                                          "snotel",
+                                                          "daymet_tiles",
+                                                          "kopRas",
+                                                          "DAY", "site_no", "YEAR", "MONTH",
+                                                          "year_2000", "year_2005", "year_2010", "COUNTY", "ID",
+                                                          "PARAMETER", "Date", ".", "ap"))
+}
