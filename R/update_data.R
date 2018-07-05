eb6bc7f43575cdb8f405add83eb32f105167af37
overdodactyl <pjohns33@protonmail.com>
2018-07-05 10:12:27 -0600

    increase time frame in update function

--- R/update_data.R
+++ R/update_data.R
@@ -2,6 +2,6 @@
 #' 
 
 update_data = function() {
-  fileList = FlowlineFinder::get_nomads_filelist(num  = 4)
-  FlowlineFinder::download_nomads_rda(fileList, number = 4)
+  fileList = FlowlineFinder::get_nomads_filelist(num  = 40)
+  FlowlineFinder::download_nomads_rda(fileList, number = 40)
 }
